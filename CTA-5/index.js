bc1fc94b42635111c9a9146e5e67fb84cc1ff2c1
Rares <dobreanurares@gmail.com>
2024-04-13 10:36:30 +0300

    CTA-5

---
+++ CTA-5/index.js
@@ -0,0 +1,9 @@
+// Verificăm dacă utilizatorul este pe un dispozitiv mobil
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}
+
+// Adăugăm o clasă 'mobile' la elementul HTML dacă utilizatorul este pe un dispozitiv mobil
+if (isMobileDevice()) {
+    document.documentElement.classList.add('mobile');
+}
